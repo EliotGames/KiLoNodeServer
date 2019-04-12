2f237b3d968efe1c8c7388bb6bd16b922465e263
NazarGorokhivskiy <nazargorokhivskiy@gmail.com>
2019-04-12 16:47:46 +0300

    Added basic server with POST method

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./api/models/product');
+const bodyParser = require('body-parser');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json())
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/pages/index.html');
+});
+
+app.post('/products', (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+    isAvaiable: req.body.isAvaiable,
+  });
+  product.save().then(result => {
+    console.log(`Product saved: ${result}`);
+  });
+
+  res.status(201).json({
+    message: 'Handling POST request to products',
+    createdProduct: product
+  });
+});
+
+app.get('/test', (req, res) => {
+  res.send("Test GET request on server");
+});
+
+
+app.listen(PORT, () => {
+  console.log('Node server started');
+});
