b812bcfc88e28a07e858a2087177f15c38ee5390
NazarGorokhivskiy <nazargorokhivskiy@gmail.com>
2019-04-15 01:13:33 +0300

    added Routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const Product = require('./api/models/product');
+const products = require('./routes/products');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,73 +10,10 @@ app.use(bodyParser.json());
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 // Main Routes
+app.use('/products', products);
+
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/pages/index.html');
-});
-
-app.post('/products', (req, res, next) => {
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-    isAvaiable: req.body.isAvaiable,
-  });
-
-  product.save().then(result => {
-    res.status(200).json({
-      message: 'Handling POST request to products',
-      createdProduct: result
-    });
-  });
-});
-
-app.get('/products', (req, res, next) => {
-  Product.find()
-    .exec()
-    .then(docs => {
-      res.status(200).json(docs);
-    })
-    .catch(err => next(err));
-});
-
-app.get("/products/:productId", (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-    .exec()
-    .then(doc => {
-      if (doc) {
-        res.status(200).json(doc);
-      } else {
-        next({
-          status: 404,
-          message: "Product is not found"
-        })
-      }
-    })
-    .catch(err => {
-      next(err);
-    })
-});
-
-app.patch('/products/:productId', (req, res, next) => {
-  const oldProduct = {'_id': req.params.productId };
-  const newProduct = req.body;
-  
-  Product.findOneAndUpdate(oldProduct, newProduct, {upsert: false}, (err, doc) => {
-      if (err) return next(err);
-
-      return res.send("Succesfully saved");
-  });
-});
-
-app.delete('/products/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  Product.remove({ _id: id })
-    .exec()
-    .then(result => {
-      res.status(200).json({itemsDeleted: result.deletedCount});
-    })
-    .catch(err => next(err));
+  res.sendFile(__dirname + '/public/pages/index.html');
 });
 
 
---
+++ routes/products.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const Product = require('../db/models/product');
+
+const router = express.Router();
+
+router.post('/', (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+    isAvaiable: req.body.isAvaiable,
+  });
+
+  product.save().then(result => {
+    res.status(200).json({
+      message: 'Handling POST request to products',
+      createdProduct: result
+    });
+  });
+});
+
+router.get('/', (req, res, next) => {
+  Product.find()
+    .exec()
+    .then(docs => {
+      res.status(200).json(docs);
+    })
+    .catch(err => next(err));
+});
+
+router.get("/:productId", (req, res, next) => {
+  const id = req.params.productId;
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        next({
+          status: 404,
+          message: "Product is not found"
+        })
+      }
+    })
+    .catch(err => {
+      next(err);
+    })
+});
+
+router.patch('/:productId', (req, res, next) => {
+  const oldProduct = {'_id': req.params.productId };
+  const newProduct = req.body;
+  
+  Product.findOneAndUpdate(oldProduct, newProduct, {upsert: false}, (err, doc) => {
+      if (err) return next(err);
+
+      return res.send("Succesfully saved");
+  });
+});
+
+router.delete('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json({itemsDeleted: result.deletedCount});
+    })
+    .catch(err => next(err));
+});
+
+module.exports = router;
