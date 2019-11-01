72afd0f72b4628ca4bfa93963f413d05c5fa2d1e
NazarGorokhivskiy <nazargorokhivskiy@gmail.com>
2019-11-01 18:11:06 +0200

    enables CORS

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ if (process.env.NODE_ENV !== "production") {
 const routes = require("./routes/index");
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
