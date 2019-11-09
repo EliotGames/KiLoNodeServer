4bd8694949c43b1278baabed7778f5dd259f537d
NazarGorokhivskiy <nazargorokhivskiy@gmail.com>
2019-11-09 06:07:27 +0200

    minor fix

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "PATCH"
+  );
   next();
 });
 app.use(bodyParser.json());
