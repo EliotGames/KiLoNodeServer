9028f35127868f05a1de1f1d165384eac8c6c53c
NazarGorokhivskiy <nazargorokhivskiy@gmail.com>
2019-11-09 06:02:32 +0200

    minor fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const routes = require("./routes/index");
 const PORT = process.env.PORT || 5000;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
